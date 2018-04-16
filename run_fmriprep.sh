dec38af80a89867699e70d3ddf666196d9643749
Nichollette Acosta <nikkibytes@graces-MacBook-Pro.local>
2018-04-16 13:06:35 -0400

    edited fmriprep commad

--- run_fmriprep.sh
+++ run_fmriprep.sh
@@ -1,19 +1,26 @@
+cd Experiments/EricData/EricData/ses-wave4
+subject_id=(sub*)
+subject_id=({*-07*,*-08*,*-09*,*10*,*11*,*12*,*13*,*15*})
+cd /mydirectory
 
 sinteractive -m 24000
 singularity shell -B /projects/niblab/bids_projects:/mydirectory \
  /projects/niblab/bids_projects/Singularity_Containers/fmriprep_container.simg
-
-cd /projects/niblab/bids_proj
-
-singularity shell -B /projects/niblab/bids_projects:/mydirectory \
-/projects/niblab/bids_projects/Singularity_Containers/fmriprep_container.simg
 cd /mydirectory
-fmriprep Experiments/EricData/EricData/ses-wave4 fmriprep_run \
+for i in ${subject_id[@]}; do
+echo "Starting fmriprep for subject $i..................................................."
+id=$(echo $i | cut -f2 -d-)
+fmriprep Experiments/EricData/EricData/ses-wave4 Experiments/EricData/fmriprep_run/$i \
     participant  \
-    --participant-label 001  \
+    --participant-label $id  \
     --fs-license-file freesurfer/license.txt \
-    --nthreads 8 --omp-nthreads 16 --low-mem \
+    --omp-nthreads 4 --n_cpus 8  --low-mem \
     --ignore slicetiming  \
     --output-space T1w --template MNI152NLin2009cAsym \
     --fs-no-reconall --debug --anat-only \
     -w intermediate_results --resource-monitor --write-graph --stop-on-first-crash
+
+
+echo "...................................................finished fmriprep for subject $i"
+echo "********************************************************************************************"
+done
