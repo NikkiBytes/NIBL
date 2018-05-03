b9501c9cd950b298308073d5fc2c566ca3913c43
NikkiBytes <nicholletteacosta@gmail.com>
2018-05-03 16:25:28 -0400

    updating bids conversion

--- Containers/BIDS-Conversion/bids_conversion.sh
+++ Containers/BIDS-Conversion/bids_conversion.sh
@@ -1,57 +1,79 @@
 #!/bin/bash
 
 read -p "Enter main directory path: " main_directory
-
+read -p "Enter study name: " study_name
 read -p "Enter input data path: " input_path # get input path
 
 if [ -z $input_path ]
 then
   echo "input path is an empty string"
-  exit
+  #exit
 fi
 
 if [ -z $main_directory ]
 then
-  echo "main_directory is an empty string"
-  exit
+  echo "The main directory is an empty string"
+  #exit
 fi
 
-if [ -e "$main_directory" ]
+if [ ! -e "$main_directory" ]
 then
   echo "main directory file path doesn't exist"
-  exit
+#  exit
 fi
 
-if [ -e "$input_path" ]
+if [ ! -e "$input_path" ]
 then
-  echo "file path doesn't exist"
-  exit
+  echo "input file path doesn't exist"
+#  exit
 else
   read -p "Enter the subjects identifier[i.e. '*']: " subject_key
+
   cd $input_path
 
+
 # Get subjects
-  subjects=($subject_key)
-# Divide subjects
-  subs1=${subjects[@]::$((${#subjects[@]} / 2 ))}
-  subs2=${subjects[@]:$((${#subjects[@]} / 2 ))}
+  subjects=(${subject_key})
 
+  echo "${subjects[@]}"
 
-  cd $main_directory
+  read -p "These are the subjects we found, enter true to proceed:      " decision
 
+  if [ "$decision" = true ] ; then
+    echo "Proceeding......"
 
-# Get conversion file directory
-  read -p "Enter heuristic file path: " heuristic_path
-  read -p "Enter unique dicom path: " dicom_path
-  read -p "Enter study name: " study_name
-  read -p "Enter output directory path: " output_directory
+    subs1=${subjects[@]::$((${#subjects[@]} / 2 ))}
+    subs2=${subjects[@]:$((${#subjects[@]} / 2 ))}
 
-  if [ -e "$heuristic_path" ]
-  then
-    echo "heuristic file path doesn't exist"
-    exit
+    cd $main_directory
+
+    read -p "Enter heuristic file path: " heuristic_path
+    read -p "Enter unique dicom path: " dicom_path
+    read -p "Enter output directory path: " output_directory
+
+    if [ ! -e "$heuristic_path" ]; then
+      echo "Heurisitic path doesn't exist"
+    fi
+
+    # Start parallel process / Run BIDS
+    for f in ${subs1[@]};do
+    echo "STARTING BIDS CONVERSION ON SUBJECT: $f ................................................................"
+    id=$(echo $f | cut -f2 -d-)
+    export f
+    heudiconv -b -d $dicom_path -s $study_name -f $heuristic_path \
+    -c dcm2niix -b  -o "$output_directory/sub-${id}"
+    echo "Finished BIDSifying subject $f"
+    done &
+    for f in ${subs2[@]};do
+    id=$(echo $f | cut -f2 -d-)
+    export f
+    heudiconv -b -d $dicom_path -s $study_name -f $heuristic_path \
+    -c dcm2niix -b -o "$output_directory/sub-${id}"
+    echo "Finished BIDSifying subject $f"
+    done &
+    wait
+
+  else
+    echo "ending"
   fi
-#
-
-
 fi
