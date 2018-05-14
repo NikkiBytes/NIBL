efc7341bdeda1292caecbae51dc7cf28848ae370
NikkiBytes <nicholletteacosta@gmail.com>
2018-05-14 12:12:23 -0400

    edits to fmriprep for bevel

--- Experiments/Bevel/fmriprep.sh
+++ Experiments/Bevel/fmriprep.sh
@@ -23,7 +23,7 @@ fmriprep Experiments/Bevel/Bevel Experiments/Bevel/Bevel/$i \
     --t2s-coreg --bold2t1w-dof 12 \
     --output-space T1w --template MNI152NLin2009cAsym \
     --debug \
-    -w Experiments/Bevel/Bevel/intermediate_results --resource-monitor --write-graph --stop-on-first-crash
+    -w Experiments/Bevel/$i/intermediate_results --resource-monitor --write-graph --stop-on-first-crash
 
 echo "...................................................finished fmriprep for subject $i"
 echo "********************************************************************************************"
@@ -32,7 +32,7 @@ done &
 for i in ${subs2[@]}; do
 echo "Starting fmriprep for subject $i..................................................."
 id=$(echo $i | cut -f2 -d-)
-fmriprep Experiments/Bevel/Bevel Experiments/Bevel/Bevel/$i \
+fmriprep Experiments/Bevel/Bevel Experiments/Bevel/fmriprep/$i \
     participant  \
     --participant-label $i  \
     --fs-license-file freesurfer/license.txt \
@@ -42,7 +42,7 @@ fmriprep Experiments/Bevel/Bevel Experiments/Bevel/Bevel/$i \
     --t2s-coreg --bold2t1w-dof 12 \
     --output-space T1w --template MNI152NLin2009cAsym \
     --debug  \
-    -w Experiments/Bevel/Bevel/intermediate_results --resource-monitor --write-graph --stop-on-first-crash
+    -w Experiments/Bevel/$i/intermediate_results --resource-monitor --write-graph --stop-on-first-crash
 
 echo "...................................................finished fmriprep for subject $i"
 echo "********************************************************************************************"
