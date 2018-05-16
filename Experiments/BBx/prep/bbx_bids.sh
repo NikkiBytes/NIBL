7183e10b5a1eff60a1b92434f088f272d712ef46
NikkiBytes <nicholletteacosta@gmail.com>
2018-05-16 12:07:24 -0400

    pushing from work computer

---
+++ Experiments/BBx/prep/bbx_bids.sh
@@ -0,0 +1,25 @@
+
+cd Raw_Data/BBx
+subjects=(sub*)
+
+subs1=${subjects[@]::$((${#subjects[@]} / 2 ))}
+subs2=${subjects[@]:$((${#subjects[@]} / 2 ))}
+
+cd /test
+for f in ${subs1[@]};do
+id=$(echo $f | cut -f2 -d-)
+echo "STARTING BIDS CONVERSION ON SUBJECT: $f ................................................................"
+export id
+heudiconv -b -d Raw_Data/{subject}/$f/*dcm -s BBx -f ConversionFiles/bbx_converter.py \
+-c dcm2niix -b  -o Experiments/BBx/BBx/$f
+echo "Finished BIDSifying subject $f"
+done &
+for f in ${subs2[@]};do
+echo "STARTING BIDS CONVERSION ON SUBJECT: $f ................................................................"
+id=$(echo $f | cut -f2 -d-)
+export id
+heudiconv -b -d Raw_Data/{subject}/$f/*dcm -s BBx -f ConversionFiles/bbx_converter.py \
+-c dcm2niix -b  -o Experiments/BBx/BBx/$f
+echo "Finished BIDSifying subject $f"
+done &
+wait
