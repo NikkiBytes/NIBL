2c2fdf76459756258ea9205d5e14c4a0fa3b6c9c
NikkiBytes <nicholletteacosta@gmail.com>
2018-06-19 17:56:40 -0400

    added correct funcrun parser

--- my_scripts/test_makefsf.py
+++ my_scripts/test_makefsf.py
@@ -26,7 +26,7 @@ def set_parser():
     
     parser.add_argument('-evs',dest='EV',nargs='+',
                         default=False, help='which evs are we using?')
-    parser.add_argument('-run',dest='RUN',nargs='+',
+    parser.add_argument('-runs',dest='RUN',nargs='+',
                         default=False, help='which run are we using?')    
     args = parser.parse_args()
     arglist={}
@@ -53,6 +53,35 @@ def set_dict():
 def fill_dict(sub):
     
     repl_dict.update({'SUB':sub})
+
+    for run in arglist['RUN']:
+        scan=(os.path.join(sub,'func','%s_task-%s_run-%s_bold_brain.nii.gz')%(sub,arglist['TASK'], run))
+       # funcrun=os.path.join(basedir,scan)
+        x=int(run)
+        repl_dict.update({'FUNCRUN%i'%x:scan})
+    
+    """
+    ntmpts=check_output(['fslnvols',funcrun])
+    repl_dict.update({'NTIMEPOINTS':ntmpts})
+
+    # TRS FROM NIFTI
+    trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
+    print(trs)
+    repl_dict.update({'TRS':trs})
+
+
+    # OUTPUT
+    output=os.path.join(deriv_dir, sub, 'func', 'Analysis', 'task', arglist['TASK'])
+    repl_dict.update({'OUTPUT':output})
+    
+    # ANAT
+    anat=os.path.join(deriv_dir ,sub,'anat','highres001*.nii.gz')
+    repl_dict.update({'ANAT':anat})
+
+    # CONFOUNDS
+    confounds=os.path.join(deriv_dir,sub,'func','motion_assessment','%s_task-%s_bold_brain_confound.txt'%(sub,arglist['TASK']))
+    repl_dict.update({'CONFOUNDS':confounds})
+
     ctr=0
     for item in arglist['EV']:
         print(item)
@@ -64,12 +93,9 @@ def fill_dict(sub):
         for i in range(6):
             motcor=os.path.join(sub,'func','motion_assessment','%s_task-%s_bold_brain_motcor%i.txt' %(sub,arglist['TASK'],i))
             repl_dict.update({'MOTCOR%i'%i:motcor})
-    # FUNCRUN --
-   # scan=(os.path.join('derivatives', sub,'func','%s_task-%s_run-%s_bold_brain.nii.gz')%(sub,arglist['TASK'], arglist['RUN']))
-    #print(scan)
-    #funcrun = os.path.join(deriv_dir, sub, scan)
-    #repl_dict.update({'FUNCRUN':funcrun})
-    
+
+
+    """
 
 set_dict()
 set_parser()
