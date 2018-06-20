d511147ff40305ee13325f51a24c549b42e069dd
NikkiBytes <nicholletteacosta@gmail.com>
2018-06-20 19:51:39 -0400

    downloaded correct get_moco file for python

--- my_scripts/get_mocos.py
+++ my_scripts/get_mocos.py
@@ -1,6 +1,16 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
 import pandas as pd
 import os
 import glob
+
+
+# In[17]:
+
 
 # get the data / directory
 def get_subjects():
@@ -12,19 +22,27 @@ def get_subjects():
     os.chdir(fmriprep_dir)
     subjects=glob.glob("sub-*")
 
-def write_files(task, run, moco_df, outputdir):
+
+# In[18]:
+
+
+def write_files(task, run, moco_df, outputdir, sub):
     for col in moco_df.columns:
         if task != 'task-rest':
-            filename = "%s_%s.txt"%(col, run)
+            filename = "%s_%s_%s_%s.txt"%(sub, task, run, col)
         else:
-            filename = "%s_rest.txt"%(col)
+            filename = "%s_task-rest.txt"%(sub)
         output_path=os.path.join(outputdir, filename)
         print("Writing to file, ", output_path)
         moco_df[col].to_csv(output_path, header=False, index=False)
 
-def get_data():
+
+# In[19]:
+
+
+def get_data(): 
     for sub in subjects:
-                #print(sub)
+        #print(sub)
         filepath=os.path.join(basedir, 'fmriprep', sub, 'fmriprep', sub, 'func')
         outputdir=os.path.join(basedir, 'derivatives', sub, 'func', 'motion_assessment')
         if not os.path.exists(os.path.join(outputdir, 'motion_parameters')):
@@ -32,30 +50,39 @@ def get_data():
         outputdir=os.path.join(outputdir, 'motion_parameters')
         os.chdir(filepath)
         for run in glob.glob("*confounds.tsv"):
-               # print("FILE: ", run)
+       # print("FILE: ", run)
             df = pd.read_table(run)
             moco_df=df[['X', 'Y', 'Z', 'RotX', 'RotY', 'RotZ']]
-            moco_df.columns = ['moco0', 'moco1', 'moco2', 'moco3', 'moco4', 'moco5']
-                #print(moco_df.head())
+            moco_df.columns = ['moco0', 'moco1', 'moco2', 'moco3', 'moco4', 'moco5'] 
+        #print(moco_df.head())
             name=run.split('_')
-            #    print(task)
+    #    print(task)
             for word in name:
                 if 'task' in word:
                     task=word
-                if task == "task-rest":
-                    run=None
-                    write_files(task, run, moco_df, outputdir)
-                else:
-                    for word in name:
-                        if 'run' in word:
-                            run=word
-                    write_files(task, run, moco_df,outputdir)
-        #        print("TASK: ", task)
-        #        print("RUN: ", run)
+            if task == "task-rest":
+                run=None
+                write_files(task, run, moco_df, outputdir,sub)
+            else:
+                for word in name:
+                    if 'run' in word:
+                        run=word
+                write_files(task, run, moco_df,outputdir,sub)
+#        print("TASK: ", task)
+#        print("RUN: ", run)
+
+
+# In[20]:
 
 
 def main():
     get_subjects()
     get_data()
+    
+
+
+# In[21]:
+
 
 main()
+
