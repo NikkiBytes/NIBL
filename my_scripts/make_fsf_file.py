b21d60f6469e00d0a71b1e2290edd8995c032268
NikkiBytes <nicholletteacosta@gmail.com>
2018-06-20 18:35:26 -0400

    progress to make fsf file it now has gathered correct params needs dictionary

--- my_scripts/make_fsf_file.py
+++ my_scripts/make_fsf_file.py
@@ -15,31 +15,33 @@ import argparse
 
 
 def set_parser():
-    global parser
-    global arglist
+  global parser
+  global arglist
 
-    parser=argparse.ArgumentParser(description='make your fsf files')
-    parser.add_argument('-noreg',dest='NOREG', action='store_true',
+  parser=argparse.ArgumentParser(description='make your fsf files')
+  parser.add_argument('-noreg',dest='NOREG', action='store_true',
                         default=False, help='Did you already register your data (using ANTZ maybe)?')
-    parser.add_argument('-task',dest='TASK',
+  parser.add_argument('-task',dest='TASK',
                         default=False, help='which task are we using?')
-    parser.add_argument('-evs',dest='EV',nargs='+',
+  parser.add_argument('-evs',dest='EV',nargs='+',
                         default=False, help='which evs are we using?')
-    args = parser.parse_args()
-    arglist={}
-    for a in args._get_kwargs():
-        arglist[a[0]]=a[1]
-    print(arglist)
+  args = parser.parse_args()
+  arglist={}
+  for a in args._get_kwargs():
+      arglist[a[0]]=a[1]
+      #print(arglist)
 
 def set_paths():
     global basedir
-    global deriv_dir
     global outdir
     #basedir = input("Enter directory path of your subjects: ")
     #outdir = input("Enter directory path for your output: ")
-    basedir='/Users/nikkibytes/Documents/Test'
-    deriv_dir=os.path.join(basedir, 'derivatives')
+    basedir='/Users/nikkibytes/Documents/testing/derivatives'
+    #deriv_dir=os.path.join(basedir, 'derivatives')
     outdir=os.path.join(deriv_dir,'task')
+
+
+
 
 # Set Dictionary keys
 
@@ -83,53 +85,44 @@ def check_registartion(sub):
 
 
 def fill_dict(sub):
-    
     repl_dict.update({'SUB':sub})
 
-    # FUNCRUN --
     scan=(os.path.join(sub,'func','%s_task-%s_bold_brain.nii.gz')%(sub,arglist['TASK']))
-    funcrun = os.path.join(deriv_dir, sub, scan)
+
+    funcrun=os.path.join(basedir,scan)
     repl_dict.update({'FUNCRUN':funcrun})
 
-
-    # NUMBER OF TIME POINTS
     ntmpts=check_output(['fslnvols',funcrun])
     repl_dict.update({'NTIMEPOINTS':ntmpts})
 
-    # TRS FROM NIFTI
     trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
     print(trs)
     repl_dict.update({'TRS':trs})
 
 
-    # OUTPUT
-    output=os.path.join(deriv_dir, sub, 'func', 'Analysis', 'task', arglist['TASK'])
+
+    output=os.path.join(outdir,sub,'grace_edit',arglist['TASK'])
     repl_dict.update({'OUTPUT':output})
-    
-    # ANAT
-    anat=os.path.join(deriv_dir ,sub,'anat','highres001*.nii.gz')
+    anat=os.path.join(basedir,sub,'anat','%s_T1w_brain.nii.gz'%(sub))
     repl_dict.update({'ANAT':anat})
 
-    # CONFOUNDS
-    confounds=os.path.join(deriv_dir,sub,'func','motion_assessment','%s_task-%s_bold_brain_confound.txt'%(sub,arglist['TASK']))
+    confounds=os.path.join(basedir,sub,'func','motion_assessment','%s_task-%s_bold_brain_confound.txt'%(sub,arglist['TASK']))
     repl_dict.update({'CONFOUNDS':confounds})
     ctr=0
     for item in arglist['EV']:
         print(item)
         ctr=ctr+1
         repl_dict.update({'EV%iTITLE'%ctr:item})
-        ev=os.path.join(deriv_dir,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item))
+        ev=os.path.join(basedir,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item))
         repl_dict.update({'EV%i'%ctr:ev})
     for i in range(6):
-        motcor=os.path.join(deriv_dir,sub,'func','motion_assessment', 'motion_parameters', '%s_task-%s_bold_brain_motcor%i.txt' %(sub,arglist['TASK'],i))
+        motcor=os.path.join(basedir,sub,'func','motion_assessment','%s_task-%s_bold_brain_motcor%i.txt' %(sub,arglist['TASK'],i))
         repl_dict.update({'MOTCOR%i'%i:motcor})
 
 
 def create_fsf():
-    os.chdir(deriv_dir)   
-    #asssuming we are in base directory
+        #asssuming we are in base directory
     for sub in glob.glob('sub-*'):
-        print(sub)
         fill_dict(sub)
         check_registartion(sub)
 
--- my_scripts/test_makefsf.py
+++ my_scripts/test_makefsf.py
@@ -36,70 +36,98 @@ def set_parser():
 
 
 
-def set_dict():
-    global repl_dict
+def set_dict(sub):
     
-    repl_dict = {
-                'SUB': None,
-                'FUNCRUN': None,
-                'NTIMEPOINTS': None,
-                'TRS': None,
-                'OUTPUT': None,
-                'ANAT': None,
-                'CONFOUNDS': None,
+    main_dict[sub] = {
+            'SUB': None,
+            'FUNCRUN': None,
+            #'NTIMEPOINTS': None,
+            'TR': None,
+            #'OUTPUT': None,
+            'ANAT': None,
+            #'CONFOUNDS': None,
          }
 
-
-def fill_dict(sub):
+############################################################################################################
+# FILL DICTIONARY METHOD                            
+# here we are updating our dictionary with relevant values
+############################################################################################################
     
-    repl_dict.update({'SUB':sub})
+def fill_dict( ):
+   
+    for sub in main_dict:
+        print(sub)
+        
+    # -- THE SUBEJCT 
+    #repl_dict.update({'SUB':sub})
+        deriv_dir = '/Users/nikkibytes/Documents/Test/derivatives'
+    # -- THE RUNS
+        for run in arglist['RUN']:
+            scan=(os.path.join( sub,'func','%s_task-%s_run-%s_bold_brain.nii.gz')%(sub,arglist['TASK'], run))
+            funcrun=os.path.join(deriv_dir, scan)
+            x=int(run)
+        #repl_dict.update({'FUNCRUN%i'%x:funcrun})
+            print("FUNCRUN: ", funcrun)
+        
+    # -- THE TIMEPOINTS -- found by running 'fslnvols' on our scan file
+            ntmpts=check_output(['fslnvols', funcrun])
+            ntmpts = ntmpts.decode('utf-8')
+            ntmpts = ntmpts.strip('\n')
+       # repl_dict.update({'NTIMEPOINTS%i'%x:ntmpts})
+            print("TIMEPOINT: ", ntmpts)
 
-    for run in arglist['RUN']:
-        scan=(os.path.join(sub,'func','%s_task-%s_run-%s_bold_brain.nii.gz')%(sub,arglist['TASK'], run))
-       # funcrun=os.path.join(basedir,scan)
-        x=int(run)
-        repl_dict.update({'FUNCRUN%i'%x:scan})
+    # -- CONFOUNDS 
+            confounds=os.path.join(deriv_dir,sub,'func','motion_assessment','%s_task-%s_run-%s_bold_brain_confound.txt'%(sub,arglist['TASK'],x))
+      #  repl_dict.update({'CONFOUNDS%i'%x:confounds})
+            print("CONFOUNDS: ", confounds)
+        
+    # -- MOTION CORRECTION
+            for i in range(6):
+                motcor=os.path.join(sub,'func','motion_assessment', 'motion_parameters','%s_task-%s_run-%s_moco%s.txt' %(sub,arglist['TASK'],run,i))
+     #       repl_dict.update({'MOTCOR%i'%i:motcor})
+                print("MOTCOR: ", motcor)
+
+
+    # -- TRS FROM NIFTI -- this value will always be 2, therefore we only run the check once
+        trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
+        trs=trs.decode('utf-8')
+        trs=trs.strip('\n')
+        print("TRs: ", trs)
+    #repl_dict.update({'TR':trs})
     
-    """
-    ntmpts=check_output(['fslnvols',funcrun])
-    repl_dict.update({'NTIMEPOINTS':ntmpts})
-
-    # TRS FROM NIFTI
-    trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
-    print(trs)
-    repl_dict.update({'TRS':trs})
-
-
-    # OUTPUT
-    output=os.path.join(deriv_dir, sub, 'func', 'Analysis', 'task', arglist['TASK'])
-    repl_dict.update({'OUTPUT':output})
     
-    # ANAT
-    anat=os.path.join(deriv_dir ,sub,'anat','highres001*.nii.gz')
-    repl_dict.update({'ANAT':anat})
-
-    # CONFOUNDS
-    confounds=os.path.join(deriv_dir,sub,'func','motion_assessment','%s_task-%s_bold_brain_confound.txt'%(sub,arglist['TASK']))
-    repl_dict.update({'CONFOUNDS':confounds})
-
-    ctr=0
-    for item in arglist['EV']:
-        print(item)
-        ctr=ctr+1
-        repl_dict.update({'EV%iTITLE'%ctr:item})
-        ev=os.path.join(sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item))
-        print(ev)
-        repl_dict.update({'EV%i'%ctr:ev})
-        for i in range(6):
-            motcor=os.path.join(sub,'func','motion_assessment','%s_task-%s_bold_brain_motcor%i.txt' %(sub,arglist['TASK'],i))
-            repl_dict.update({'MOTCOR%i'%i:motcor})
+    # -- OUTPUT -- directory where output will go
+        output=os.path.join(deriv_dir, sub, 'func', 'Analysis', 'task', arglist['TASK'])
+    #repl_dict.update({'OUTPUT':output})
+        print("OUTPUT: ", output)   
+    
+    
+    # -- ANAT -- get the anat file for the subject, the syntax currently follows fmriprep standard
+        anat=os.path.join(deriv_dir ,sub,'anat','highres001_BrainExtractionBrain.nii.gz')
+    #repl_dict.update({'ANAT':anat})
+        print("ANAT: ", anat)
 
 
-    """
+    # -- EVS -- here we loop through the given EVs and add the corresponding file to the dictionary
 
-set_dict()
+        ctr=0
+        for item in arglist['EV']:
+            #print(item)
+            ctr=ctr+1
+            #repl_dict.update({'EV%iTITLE'%ctr:item})
+            ev=os.path.join(sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item))
+            print("EV: ", ev)
+            #repl_dict.update({'EV%i'%ctr:ev})
+        
+
+
+global main_dict
+main_dict = {}
+
+
+set_dict('sub-001')
+
 set_parser()
-fill_dict('sub-001')
+fill_dict()
 
 #print("ARGLIST: ", arglist)
-print("DICT: ", repl_dict)
