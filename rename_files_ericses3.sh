2bdc01cce4fa2074b7d958e37ea46fedc9b8e9f4
gshearrer <grace.shearer@gmail.com>
2018-06-28 16:51:25 -0400

    adding

--- rename_files_ericses3.sh
+++ rename_files_ericses3.sh
@@ -2,4 +2,18 @@ for sub in ${subs[@]}; do
 cd /projects/niblab/bids_projects/Experiments/EricData/data/BIDS/EricData/wave3/$sub/func
 for f in *ses*; do mv "$f" "${f//ses-2/ses-3}";  done
 ls
-done 
+done
+
+
+subs=(sub-*)
+for i in ${subs[@]}; do
+    sub=$(echo $i | cut -f1 -d_)
+    mv $i /projects/niblab/bids_projects/Experiments/Bevel/data/derivatives/$sub/func/onsets
+done
+
+for sub in ${subs[@]}; do
+  cd /projects/niblab/bids_projects/Raw_Data/Eric_Data/wave2
+  while IFS=" " read -r val1 val2
+  do
+  mv $val1 "sub-$val2"
+done < "EricData.txt"
