7643a9228700285dec2b9e6983aab8d24211cd9a
NikkiBytes <nicholletteacosta@gmail.com>
2018-07-09 15:09:11 -0400

    rename script

--- rename_onsets.py
+++ rename_onsets.py
@@ -1,13 +1,44 @@
 import glob
 import os
-path = "/Users/nikkibytes/Documents/testing/BIDS"
-bevel_txt = "/Users/nikkibytes/Documents/testing/Bevel/Bevel.txt"
+import argparse
 
+
+
+path = '/projects/niblab/bids_projects/Experiments/Bevel/data'
+derivpath= os.path.join(path, 'derivatives')
+bevel_txt = os.path.join(path, 'Bevel.txt')
+print(derivpath)
+
+
+
+#path = "/projects/niblab/bids_projects/Experiments/Bevel/data/Bevel"
+#bevel_txt = "/projects/niblab/bids_projects/Experiments/Bevel/data/Bevel.txt"
+
+
+
+
+
+
+def set_parser():
+
+    global arglist
+
+    parser=argparse.ArgumentParser(description='make your fsf files')
+    parser.add_argument('-bids',dest='BIDS', action='store_true',
+                        default=False, help='rename BIDS?')
+    parser.add_argument('-deriv',dest='DERIV', action='store_true',
+                        default=False, help='rename derivatives?')
+
+    args = parser.parse_args()
+    arglist={}
+    for a in args._get_kwargs():
+        arglist[a[0]]=a[1]
 
 # Dictionary 
 
 def getConversionDict():
     global conversion
+
     conversion = {}
     with open(bevel_txt, 'r') as f:
         for line in f:
@@ -17,43 +48,117 @@ def getConversionDict():
 
 
 
-def parseFile():
+def renameBIDS():
     os.chdir(path)
-    subs = glob.glob('sub-*')
-    print(subs)
+    subs = glob.glob('sub-*')    
+    
+    # replace individual files
+    for dir_ in subs:
+        print(dir_)
+        curr_sub = dir_
+        new_sub = "sub-0"+conversion[curr_sub]
+        
+        #print(curr_sub, new_sub)
+        
+        os.chdir(os.path.join(path,dir_,'fmap'))
+        fmaps = glob.glob('sub-*')
+
+        for file in fmaps:
+            #print("ORIGINAL", file)
+            new = file.replace(curr_sub, new_sub)
+            print("ORIGINAL FMAP:", file)
+            print("NEW FMAP: ", new)
+            os.replace(file, new)
+        
+        
+        os.chdir(os.path.join(path,dir_,'func'))
+        funcs = glob.glob('sub-*')
+
+        for file in funcs:
+            new = file.replace(curr_sub, new_sub)
+            print("ORIGINAL FUNC:", file)
+            print("NEW FUNC:", new)
+            os.replace(file, new)
+
+        
+        os.chdir(os.path.join(path,dir_,'anat'))
+        anats = glob.glob('sub-*')
+
+        for file in anats:
+            new = file.replace(curr_sub, new_sub)
+            print("ORIGINAL ANAT: ", file)
+            print("NEW ANAT: ", new)
+            os.replace(file, new)
+            
+            
+        os.chdir(os.path.join(path, dir_))
+        subdir = glob.glob("*%s*"%dir_)
+        for file in subdir:
+            new = file.replace(curr_sub, new_sub)
+            print(file)
+            print(new)
+            os.replace(file, new)
+  
+def renameDir(input_path):
+          
+    os.chdir(input_path)
+
+    subs = glob.glob('sub-*')    
     for dir_ in subs:
         curr_sub = dir_
+        new_sub = "sub-0"+conversion[curr_sub]
+        os.replace(dir_, new_sub)
+    
+def renameDeriv():
+    os.chdir(derivpath)
+    subs = glob.glob('sub-*')   
+    
+    
+    for dir_ in subs:
+        print(dir_)
+        curr_sub = dir_
+        new_sub = "sub-0"+conversion[curr_sub]
         
-        fmaps = glob.glob(os.path.join(dir_,'fmap', 'sub-*'))
-        funcs = glob.glob(os.path.join(dir_,'func', 'sub-*'))
-        anats = glob.glob(os.path.join(dir_,'anat', 'sub-*'))
+        os.chdir(os.path.join(derivpath,dir_, 'func'))
+        funcs = glob.glob("*%s*"%dir_) 
         
-        
-        for file in fmaps:
+        for file in funcs:
             print(file)
+            new = file.replace(curr_sub, new_sub)
+            print("-------> ",new)
+            os.replace(file, new)
+
+        os.chdir('motion_assessment')
+        confounds = glob.glob("*%s*"%dir_)
         
-        #for file in fmaps:
-         #   orgname = 
-        print(fmaps)
-        print("________________________________________")
-        print(funcs)
-        print("________________________________________")
-        print(anats)
+        for file in confounds:
+            print(file)
+            new = file.replace(curr_sub, new_sub)
+            print("-------> ",new)
+            os.replace(file, new)
+            
+            
+        os.chdir('motion_parameters')
+        mocos = glob.glob("*%s*"%dir_)
         
-    '''for onset in files:
-        orig_name = onset.split('/')[8]
-        print("ORIGINAL: ", orig_name)
-        orig_ID = orig_name.split('_')[0].split('l')[1]
-    #print(orig_ID)
-        newID = conversion[orig_ID]
-        #print(newID)
-        task = orig_name.split('_')[1]
-    #print(task)
-        run = orig_name.split('_')[2].split('0')[1].split('.')[0]
-        #print(run)
-        new_name = "%s_task-%s_run-%s.txt"%(newID, task, run)
-        print("NEW: ", new_name)
-        '''
+        for file in mocos:
+            print(file)
+            new = file.replace(curr_sub, new_sub)
+            print("-------> ",new)
+            os.replace(file, new)
+        
+        
+        
+
+set_parser()        
 getConversionDict()
-parseFile()
-conversion
+
+if arglist["BIDS"] == True:
+    print("here")
+    renameBIDS()
+    renameDir()
+
+if arglist["DERIV"] == True:
+    print('here in deriv')  
+    renameDeriv()
+    renameDir(derivpath)
