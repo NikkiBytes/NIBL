24987cf6ee0b6418880455d2ec866d681992661b
NikkiBytes <nicholletteacosta@gmail.com>
2018-08-27 12:22:56 -0400

    added

--- Experiments/BBx/bbx_fmriprep.sh
+++ Experiments/BBx/bbx_fmriprep.sh
@@ -17,57 +17,59 @@ echo "Starting fmriprep: " $(date)
 START=$(date)
 
 for i in ${subs1[@]}; do
-echo "Starting fmriprep for subject $i..................................................."
+echo ">>>>>>------------------------------------>STARTING FMRIPREP ON SUBJECT $i"
 id=$(echo $i | cut -f2 -d-)
-fmriprep Experiments/BevBits/data/BIDS/ses-1 Experiments/BevBits/fmriprep2/ses-1/$iexp \
+fmriprep Experiments/BBx/BIDS/ses-1 Experiments/bbx/fmriprep/ses-1/$i \
     participant  \
     --participant-label $i  \
     --fs-license-file freesurfer/license.txt \
     --fs-no-reconall \
-    --omp-nthreads 30 --n_cpus 60 \
+    --omp-nthreads 30 --n_cpus 6 \
     --bold2t1w-dof 12 \
-    --output-space T1w --template MNI152NLin2009cAsym \
+    --output-space template --template MNI152NLin2009cAsym \
     --debug  --ignore slicetiming \
-    -w Experiments/BBx/fmriprep/$i/intermediate_results \
+    -w Experiments/bbx/fmriprep/$i \
     --resource-monitor --write-graph --stop-on-first-crash
 
-echo "...................................................finished fmriprep for subject $i"
+echo ">>>>>>------------------------------------>FINISHING FMRIPREP FOR SUBJECT $i"
 echo "********************************************************************************************"
 done &
 for i in ${subs2[@]}; do
-echo "Starting fmriprep for subject $i..................................................."
+echo ">>>>>>------------------------------------>STARTING FMRIPREP ON SUBJECT $i"
 id=$(echo $i | cut -f2 -d-)
-fmriprep Experiments/BevBits/data/BIDS/ses-1 Experiments/BevBits/fmriprep2/ses-1/$i \
+fmriprep Experiments/BBx/BIDS/ses-1 Experiments/bbx/fmriprep/ses-1/$i \
     participant  \
     --participant-label $i  \
     --fs-license-file freesurfer/license.txt \
     --fs-no-reconall \
-    --omp-nthreads 30 --n_cpus 60 \
+    --omp-nthreads 30 --n_cpus 6 \
     --bold2t1w-dof 12 \
-    --output-space T1w --template MNI152NLin2009cAsym \
-    --debug --ignore slicetiming \
-    -w Experiments/BBx/fmriprep/$i/intermediate_results \
+    --output-space template --template MNI152NLin2009cAsym \
+    --debug  --ignore slicetiming \
+    -w Experiments/bbx/fmriprep/$i \
     --resource-monitor --write-graph --stop-on-first-crash
 
-echo "...................................................finished fmriprep for subject $i"
+
+echo ">>>>>>------------------------------------>FINISHING FMRIPREP FOR SUBJECT $i"
 echo "********************************************************************************************"
 done &
 for i in ${subs3[@]}; do
-echo "Starting fmriprep for subject $i..................................................."
+echo ">>>>>>------------------------------------>STARTING FMRIPREP ON SUBJECT $i"
 id=$(echo $i | cut -f2 -d-)
-fmriprep Experiments/BevBits/data/BIDS/ses-1 Experiments/BevBits/fmriprep2/ses-1/$i \
+fmriprep Experiments/BBx/BIDS/ses-1 Experiments/bbx/fmriprep/ses-1/$i \
     participant  \
     --participant-label $i  \
     --fs-license-file freesurfer/license.txt \
     --fs-no-reconall \
-    --omp-nthreads 30 --n_cpus 60 \
+    --omp-nthreads 30 --n_cpus 6 \
     --bold2t1w-dof 12 \
-    --output-space T1w --template MNI152NLin2009cAsym \
+    --output-space template --template MNI152NLin2009cAsym \
     --debug  --ignore slicetiming \
-    -w Experiments/BBx/fmriprep/$i/intermediate_results \
+    -w Experiments/bbx/fmriprep/$i \
     --resource-monitor --write-graph --stop-on-first-crash
 
-echo "...................................................finished fmriprep for subject $i"
+
+echo ">>>>>>------------------------------------>FINISHING FMRIPREP FOR SUBJECT $i"
 echo "********************************************************************************************"
 done &
 wait
@@ -75,6 +77,5 @@ wait
 echo "Started fmriprep at: " $START
 echo "Finished fmriprep for BBx at: " $(date -u)
 FINISHED=$(date -u)
-exit
 exit
 exit
