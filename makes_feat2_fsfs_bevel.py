5e3cb987a189b2733ce9927176403d6081d721b4
gshearrer <grace.shearer@gmail.com>
2018-09-20 11:48:14 -0400

    added feat2 files

--- makes_feat2_fsfs_bevel.py
+++ makes_feat2_fsfs_bevel.py
@@ -1,29 +1,30 @@
 import glob
 import os
-DER_DIR = "/projects/niblab/bids_projects/Experiments/bbx/derivatives"
-SUB_DIR = "/projects/niblab/bids_projects/Experiments/bbx/derivatives/sub-*"
+DER_DIR = "/projects/niblab/bids_projects/Experiments/Bevel/data/derivatives"
+SUB_DIR = "/projects/niblab/bids_projects/Experiments/Bevel/data/derivatives/sub-*"
 SUB_DIRS = glob.glob(SUB_DIR)
 
 for sub in SUB_DIRS:
     subject = sub.split("/")[-1]
     #check for existence of feat2 directory
-    FEAT2_DIR = os.path.join(sub, "ses-1/func/Analysis/feat2")
+    FEAT2_DIR = os.path.join(sub, "func/Analysis/feat2")
     if os.path.exists(FEAT2_DIR):
         pass
     else:
         os.makedirs(FEAT2_DIR)
     print("> STARTING PROGRAM......")
     print("----------------------->>>>SUBJECT: ",sub)
-    FEATS_PATH = os.path.join(sub, "ses-1/func/Analysis/feat1/*.feat")
+    FEATS_PATH = os.path.join(sub, "func/Analysis/feat1/*.feat")
     FEATS = glob.glob(FEATS_PATH)
-    with open(os.path.join(DER_DIR, "design.fsf"), 'r') as infile:
+    with open(os.path.join(DER_DIR, "feat2_design_bevel.fsf"), 'r') as infile:
         tempfsf=infile.read()
         # set outpath for fsf OUTPATH variable, by run
-        outpath = os.path.join(sub, "ses-1/func/Analysis/feat2")
+        outpath = os.path.join(sub, "func/Analysis/feat2")
         print(">>>>>>>>>>>>>>>>>SETTING DESIGN OUTPATH: ", outpath)
         tempfsf = tempfsf.replace("OUTPUT", outpath)
+        print(FEATS)
         for run_path in FEATS:
-            run = run_path.split("/")[-1].split(".")[0].split("_")[1]
+            run = run_path.split("/")[-1].split(".")[0]
             print(">>>>>------> RUN: ", run)
             if run == "run1":
                 print("> %s : %s"%(run, run_path))
