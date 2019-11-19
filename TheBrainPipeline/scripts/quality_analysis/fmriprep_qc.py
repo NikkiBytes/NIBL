f36710f7a18cf2e9890f5fc9467bce2d43d61150
nikkibytes <nicholletteacosta@gmail.com>
2019-11-19 17:46:58 -0500

    added qc reporting

--- TheBrainPipeline/scripts/quality_analysis/fmriprep_qc.py
+++ TheBrainPipeline/scripts/quality_analysis/fmriprep_qc.py
@@ -1,5 +1,9 @@
 import glob
 import os
+import jinja2
+
+
+
 
 def get_data(sub_id, study_path, ses_id, sub_dict):
     fmriprep_path = os.path.join(study_path, "bids/derivatives/fmriprep")
@@ -24,11 +28,26 @@ def get_data(sub_id, study_path, ses_id, sub_dict):
     sub_dict[sub_id]["FUNC_SDCS"] = func_sdcs
     sub_dict[sub_id]["FUNC_FLIRTS"] = func_flirts
 
+def make_html(sub_dict):
+    env = jinja2.Environment(loader=jinja2.PackageLoader('app'))
+    template = env.get_template('base.html')
+
+    filename = os.path.join("html/base.html")
+
+    with open(filename, "w") as fh:
+        fh.write(template.render(
+            Title = "BBx Session 1 fmriprep QC",
+            data_dict = sub_dict
+
+        ))
+
+
+
 
 def main():
     # Get base path, and check for existing QC file.
     #study_path = "/projects/niblab/bids_projects/Experiments/bbx"
-    study_path = "/Users/nikkibytes/Documents/data/bbx"
+    study_path = "/qc_generator/data/bbx"
     # outfile=os.path.join(path, "bids/derivatives/bbx_ses-1_QA_fmriprep.html")
     # os.remove(outfile)
     # f = open(outfile, 'w')
@@ -36,10 +55,12 @@ def main():
     # get fmriprep path of all subjects --here we have set it to ses-2 (**may have to customize)
     sub_ids = [x.split("/")[-1] for x in glob.glob(os.path.join(study_path, 'bids/derivatives/fmriprep', 'sub-*'))]
     sub_dict = {}
-    print(sub_ids)
+    #print(sub_ids)
     for sub_id in sub_ids:
         get_data(sub_id, study_path, "1", sub_dict)
-    print(sub_dict)
+    #print(sub_dict)
+    for i in sub_dict:
+        print(sub_dict[i]["FMAP"])
     make_html(sub_dict)
 if __name__ == "__main__":
     main()
